f0a14db6ed881ea7332cd539bf98ca8b8b64b6a0
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-09 15:56:41 +0100

    Ajout de l'ouverture de départ

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 $(function(){
 
-    var $mainMenuItems = $("#main-menu ul").children("li"),   // On capture les éléments dans une variable. Les élm sont ici très précis.
-    totalMainMenuItems = $mainMenuItems.length,       // Renvoi le nbr. d'élm dans mainMenuItems
-    openedIndex = -1,   //Le code pour dire que tout le menu sera fermé. Aucun Item ouvert.
+    var $mainMenuItems = $("#main-menu ul").children("li"),         // On capture les éléments dans une variable. Les élm sont ici très précis.
+    totalMainMenuItems = $mainMenuItems.length,                     // Renvoi le nbr. d'élm dans mainMenuItems
+    openedIndex = 2,                                                //Le code pour dire que tout le menu sera fermé. Aucun Item ouvert.
 
     init = function(){
+        bindEvents();
 
-        $mainMenuItems.children(".images").click(function(){    // Quand on clique sur l'image, c-à-d l'enfant du 'li', qui a pour class .images
-            var newIndex = $(this).parent().index(),        // Le click signifie dès lors qu'on a cliqué sur un .images, mais le 'this' signifie l'instance particulière sur laquelle on a cliqué, parmi les .images.
-                                                            // On obtient l'index du li en allant chercher le parent du .images précis sur lequel on a cliqué.
-            $item = $mainMenuItems.eq(newIndex);            // Si le newIndex est n°2, Le $item sera le li avec index 2.
-            
-            if (openedIndex === newIndex)
-            {
-                animateItems($item, false, 250);            // Fermeture de l'item, si on clique 2 fois sur le même index.
-                openedIndex = -1;                           // Code signifiant qu'aucun index n'est ouvert.
-            }
-            else
-            {
-            if(validIndex(newIndex))                                                // Nous dit si l'index sur lequel on a appuyé est valid, puis execute le code qui suit.
+        if(validIndex(openedIndex))                                                 
+            animateItems($mainMenuItems.eq(openedIndex), true, 700);                  // Création de l'ouverture de départ.
+
+    }
+        bindEvents = function(){
+            $mainMenuItems.children(".images").click(function(){    // Quand on clique sur l'image, c-à-d l'enfant du 'li', qui a pour class .images
+                var newIndex = $(this).parent().index(),            // Le click signifie dès lors qu'on a cliqué sur un .images, mais le 'this' signifie l'instance particulière sur laquelle on a cliqué, parmi les .images.
+                                                                    // On obtient l'index du li en allant chercher le parent du .images précis sur lequel on a cliqué.
+                $item = $mainMenuItems.eq(newIndex);                // Si le newIndex est n°2, Le $item sera le li avec index 2.
+                
+                if (openedIndex === newIndex)
                 {
-                    animateItems($mainMenuItems.eq(openedIndex), false, 250);       // Fermeture d'item précédemment ouvert.
-                    openedIndex = newIndex;                                         // Passage de l'ancien index au nouvel index.
-                    animateItems($item, true, 250);                                 // Ouverture du nouvel item, qui est $mainMenuItems.eq(openedIndex).
+                    animateItems($item, false, 250);                // Fermeture de l'item, si on clique 2 fois sur le même index.
+                    openedIndex = -1;                               // Code signifiant qu'aucun index n'est ouvert.
                 }
-            }
-            
-        });
-    },
+                else
+                {
+                if(validIndex(newIndex))                                                // Nous dit si l'index sur lequel on a appuyé est valid, puis execute le code qui suit.
+                    {
+                        animateItems($mainMenuItems.eq(openedIndex), false, 250);       // Fermeture d'item précédemment ouvert.
+                        openedIndex = newIndex;                                         // Passage de l'ancien index au nouvel index.
+                        animateItems($item, true, 250);                                 // Ouverture du nouvel item, qui est $mainMenuItems.eq(openedIndex).
+                    }
+                }
+                
+            });
+        },
 
         validIndex = function(indexToCheck){                                        // Cette fct. nous dis si l'index est valide.
             return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
